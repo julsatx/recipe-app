80110767924817a35d04dc29c1954bbdc677ed22
Julius Camarillo <julcamarillo17@gmail.com>
2019-03-10 18:25:52 -0500

    API connected no errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props); 
+      this.state = {
+        meal: ""
+      }
+    }
+    componentDidMount(){
+      const url = 'https://www.themealdb.com/api/json/v1/1/latest.php';
+      axios.get(URL)
+      .then(res => {
+        const meal = res.data.meals; //mealdata
+        if(typeof meal ==='object'){
+          this.setState({ meal });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>HIIII</h1>
+
       </div>
     );
   }
